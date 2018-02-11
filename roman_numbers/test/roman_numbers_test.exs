989f31e5fa958f68376bffc740bc16077dbbac84
dabeat <garcia.narbona@gmail.com>
2018-02-11 20:47:18 +0100

    parameterized added

--- roman_numbers/test/roman_numbers_test.exs
+++ roman_numbers/test/roman_numbers_test.exs
@@ -1,104 +1,38 @@
 defmodule RomanNumbersTest do
   use ExUnit.Case
+  use ExUnit.Parameterized
+
   doctest RomanNumbers
 
-  test "should return 'I' when 1 is provided" do
-    assert RomanNumbers.convert(1) == "I"
-  end
-
-  test "should return 'II' when 2 is provided" do
-    assert RomanNumbers.convert(2) == "II"
-  end
-
-  test "should return 'III' when 3 is provided" do
-    assert RomanNumbers.convert(3) == "III"
-  end
-
-  test "should return 'IV' when 4 is provided" do
-    assert RomanNumbers.convert(4) == "IV"
-  end
-
-  test "should return 'V' when 5 is provided" do
-    assert RomanNumbers.convert(5) == "V"
-  end
-
-  test "should return 'VI' when 6 is provided" do
-    assert RomanNumbers.convert(6) == "VI"
-  end
-
-  test "should return 'VII' when 7 is provided" do
-    assert RomanNumbers.convert(7) == "VII"
-  end
-
-  test "should return 'VIII' when 8 is provided" do
-    assert RomanNumbers.convert(8) == "VIII"
-  end
-
-  test "should return 'X' when 10 is provided" do
-    assert RomanNumbers.convert(10) == "X"
-  end
-
-  test "should return 'XI' when 11 is provided" do
-    assert RomanNumbers.convert(11) == "XI"
-  end
-
-  test "should return 'XII' when 12 is provided" do
-    assert RomanNumbers.convert(12) == "XII"
-  end
-
-  test "should return 'XIII' when 13 is provided" do
-    assert RomanNumbers.convert(13) == "XIII"
-  end
-
-  test "should return 'XV' when 15 is provided" do
-    assert RomanNumbers.convert(15) == "XV"
-  end
-
-  test "should return 'XVI' when 16 is provided" do
-    assert RomanNumbers.convert(16) == "XVI"
-  end
-
-  test "should return 'XVII' when 17 is provided" do
-    assert RomanNumbers.convert(17) == "XVII"
-  end
-
-  test "should return 'XVIII' when 18 is provided" do
-    assert RomanNumbers.convert(18) == "XVIII"
-  end
-
-  test "should return 'XX' when 20 is provided" do
-    assert RomanNumbers.convert(20) == "XX"
-  end
-
-  test "should return 'XXX' when 30 is provided" do
-    assert RomanNumbers.convert(30) == "XXX"
-  end
-
-  test "should return 'XXXVII' when 37 is provided" do
-    assert RomanNumbers.convert(37) == "XXXVII"
-  end
-
-  test "should return 'LXXIII' when 73 is provided" do
-    assert RomanNumbers.convert(73) == "LXXIII"
-  end
-
-  test "should return 'CCLXVIII' when 268 is provided" do
-    assert RomanNumbers.convert(268) == "CCLXVIII"
-  end
-
-  test "should return 'CCCXXI' when 321 is provided" do
-    assert RomanNumbers.convert(321) == "CCCXXI"
-  end
-
-  test "should return 'MDCLXXXII' when 1682 is provided" do
-    assert RomanNumbers.convert(1682) == "MDCLXXXII"
-  end
-
-  test "should return 'MCDXCIV' when 1494 is provided" do
-    assert RomanNumbers.convert(1494) == "MCDXCIV"
-  end
-
-  test "should return 'MMMCDLXIX' when 3469 is provided" do
-    assert RomanNumbers.convert(3469) == "MMMCDLXIX"
+  test_with_params "should return expected roman number when arabic number is provided",
+    fn (arabic_number, expected_roman_number) ->      
+      assert RomanNumbers.convert(arabic_number) == expected_roman_number
+    end do
+      [
+        "should return 'I' when 1 is provided": {1, "I"},
+        "should return 'II' when 2 is provided": {2, "II"},
+        "should return 'III' when 3 is provided": {3, "III"},
+        "should return 'IV' when 4 is provided": {4, "IV"},
+        "should return 'V' when 5 is provided": {5, "V"},
+        "should return 'VII' when 7 is provided": {7, "VII"},
+        "should return 'VIII' when 8 is provided": {8, "VIII"},
+        "should return 'X' when 10 is provided": {10, "X"},
+        "should return 'XI' when 11 is provided": {11, "XI"},
+        "should return 'XII' when 12 is provided": {12, "XII"},
+        "should return 'XIII' when 13 is provided": {13, "XIII"},
+        "should return 'XV' when 15 is provided": {15, "XV"},
+        "should return 'XVI' when 16 is provided": {16, "XVI"},
+        "should return 'XVII' when 17 is provided": {17, "XVII"},
+        "should return 'XVIII' when 18 is provided": {18, "XVIII"},
+        "should return 'XX' when 20 is provided": {20, "XX"},
+        "should return 'XXX' when 30 is provided": {30, "XXX"},
+        "should return 'XXXVII' when 37 is provided": {37, "XXXVII"},
+        "should return 'LXXIII' when 73 is provided": {73, "LXXIII"},
+        "should return 'CCLXVIII' when 268 is provided": {268, "CCLXVIII"},
+        "should return 'CCCXXI' when 321 is provided": {321, "CCCXXI"},
+        "should return 'MDCLXXXII' when 1682 is provided": {1682, "MDCLXXXII"},
+        "should return 'MCDXCIV' when 1494 is provided": {1494, "MCDXCIV"},
+        "should return 'MMMCDLXIX' when 3469 is provided": {3469, "MMMCDLXIX"}
+      ]
   end
 end
