5ce51d3730828df79838bef244ceb370bc8db08d
dabeat <garcia.narbona@gmail.com>
2018-02-18 20:16:35 +0100

    format

--- fizz_buzz/lib/fizzbuzz_macros.ex
+++ fizz_buzz/lib/fizzbuzz_macros.ex
@@ -1,13 +1,13 @@
 defmodule Fizzbuzz_Macros do
   defmacro is_fizz(n) do
-    quote do: rem(unquote(n), 3) == 0  	
+    quote do: rem(unquote(n), 3) == 0
   end
 
-   defmacro is_buzz(n) do
+  defmacro is_buzz(n) do
     quote do: rem(unquote(n), 5) == 0
   end
 
-   defmacro is_fizzbuzz(n) do
+  defmacro is_fizzbuzz(n) do
     quote do: rem(unquote(n), 3) == 0 and rem(unquote(n), 5) == 0
   end
 end
--- fizz_buzz/test/fizz_buzz_test.exs
+++ fizz_buzz/test/fizz_buzz_test.exs
@@ -11,15 +11,16 @@ defmodule FizzBuzzTest do
     fun = fn ->
       FizzBuzz.list(0)
     end
-  	
-  	assert capture_io(fun) == ""    
+
+    assert capture_io(fun) == ""
   end
 
   test "should return fizz buzz list if equal or lower than 20" do
-  	fun = fn ->
+    fun = fn ->
       FizzBuzz.list(20)
     end
-  	
-  	assert capture_io(fun) == "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\n"    
+
+    assert capture_io(fun) ==
+             "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\n"
   end
 end
